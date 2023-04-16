1d445fc347bdb09079f80185eab317a5ad23d5e1
siddhant mishra <siddhantm864@gmail.com>
2023-04-16 16:05:10 +0530

    signout-dispatch

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 export const loginCall = async (userCredential, dispatch) => {
-  dispatch({ type: "LOGIN_START" });
+  // dispatch({ type: "LOGIN_START" });
   try {
     const res = await axios.post("/auth/login", userCredential);
-    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });     
   } catch (err) {
+    console.log("res",err)
     dispatch({ type: "LOGIN_FAILURE", payload: err });
   }
 };
